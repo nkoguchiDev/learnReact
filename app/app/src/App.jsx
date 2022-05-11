84614d42c5edd7e45f95887a575dea5ba9902a47
nkoguchi <nkoguchi00dev@gmail.com>
2022-05-12 00:55:18 +0900

    WIP

--- app/app/src/App.jsx
+++ app/app/src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import { ColoredMessage } from "./components/ColoredMessage";
+
 export const App = () => {
+    const [num, setNum] = useState(0);
+    const [num01, setNum01] = useState(0);
+
     const onClickButton = () => {
-        alert()
-    }
+        setNum((prev) => prev + 1);
+        setNum01(num01 + 1);
+    };
+
     return (
         <>
-            <h1>こんにちは!</h1>
-            <p>お元気ですか?</p>
+            <h1 style={{ color: "red" }}>こんにちは!</h1>
+            <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
+            <ColoredMessage color="pink">元気です</ColoredMessage>
             <button onClick={onClickButton}>ボタン</button>
+            <p>{num}</p>
+            <p>{num01}</p>
         </>
     );
 };
